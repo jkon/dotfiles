675bf1334c9e42137754a623364626b4894df9fc
jkon <joshuapkon@gmail.com>
2017-01-27 13:56:58 -0700

    Update set_language_and_region_preferences.sh

--- os/os_x/preferences/set_language_and_region_preferences.sh
+++ os/os_x/preferences/set_language_and_region_preferences.sh
@@ -10,11 +10,11 @@ set_preferences() {
     execute "defaults write NSGlobalDomain AppleLanguages -array 'en'" \
         "Set language"
 
-    execute "defaults write NSGlobalDomain AppleMeasurementUnits -string 'Centimeters'" \
-        "Set measurement units"
+   # execute "defaults write NSGlobalDomain AppleMeasurementUnits -string 'Centimeters'" \
+    #    "Set measurement units"
 
-    execute "defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false" \
-        "Disable auto-correct"
+    #execute "defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false" \
+     #   "Disable auto-correct"
 
 }
 
